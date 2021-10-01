b95b552252341a23931bb14a3c436b15e9602259
Erick Sixto <sixto.hernandez@theksquaregroup.com>
2021-09-30 21:06:01 -0500

    Finished

--- force-app/main/default/lwc/infoCardList/infoCardList.js
+++ force-app/main/default/lwc/infoCardList/infoCardList.js
@@ -4,25 +4,22 @@ export default class InfoCardList extends LightningElement {
   hasRendered = false;
 
   renderedCallback() {
-    if (!this.hasRendered) {
-      if (this.cards !== undefined) {
-        console.log(JSON.parse(JSON.stringify(this.cards)));
-        let focus = this.cards.find((card) => card.focused);
-        if (focus) {
-          let cardFocused = null;
-          let container = this.template.querySelector(".container");
-          this.template.querySelectorAll(".item").forEach((item) => {
-            if (focus.Id === item.name) {
-              cardFocused = item;
-            }
-          });
-          container.scrollLeft =
-            cardFocused.getBoundingClientRect().left -
-            container.getBoundingClientRect().left;
-        }
+    if (!this.hasRendered && this.cards !== undefined) {
+      let focus = this.cards.find((card) => card.focused);
+      if (focus) {
+        let cardFocused = null;
+        let container = this.template.querySelector(".container");
+        this.template.querySelectorAll(".item").forEach((item) => {
+          if (focus.Id === item.name) {
+            cardFocused = item;
+          }
+        });
+        container.scrollLeft =
+          cardFocused.getBoundingClientRect().left -
+          container.getBoundingClientRect().left;
       }
+      this.hasRendered = true;
     }
-    this.hasRendered = true;
   }
 
   handleClick(event) {
