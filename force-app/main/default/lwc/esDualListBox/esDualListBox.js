2270fd09ffac72b4ec18d47fb10e1e64897fd6ac
Erick Sixto <sixto.hernandez@theksquaregroup.com>
2021-10-04 19:18:28 -0500

    Selection Works. Now the flow uses 2 collections

--- force-app/main/default/lwc/esDualListBox/esDualListBox.js
+++ force-app/main/default/lwc/esDualListBox/esDualListBox.js
@@ -4,54 +4,51 @@ export default class EsDualListBox extends LightningElement {
   @api label = "DualList Label";
   @api availableColumnLabel = "Available Column Label";
   @api selectedColumnLabel = "Selected Column Label";
-  @api elements = [
+  @api required = false;
+  @api availableElements = [
     {
       Label: "English",
       Value: "123-English",
-      IconName: "utility:user",
+      IconName: "utility:clear",
       Tooltip: "Your Tooltip",
-      Disabled: false,
-      Selected: false
+      Disabled: true
     },
     {
       Label: "Italian",
       Value: "123-Italian",
       IconName: null,
       Tooltip: null,
-      Disabled: false,
-      Selected: true
+      Disabled: false
     },
     {
       Label: "French",
       Value: "123-French",
-      IconName: null,
+      IconName: "utility:ban",
       Tooltip: null,
-      Disabled: true,
-      Selected: false
+      Disabled: true
     },
     {
       Label: "German",
       Value: "123-German",
-      IconName: "utility:announcement",
+      IconName: "",
       Tooltip: null,
-      Disabled: false,
-      Selected: false
-    },
+      Disabled: true
+    }
+  ];
+  @api selectedElements = [
     {
       Label: "Korean",
       Value: "123-Korean",
       IconName: null,
       Tooltip: "Help text",
-      Disabled: true,
-      Selected: false
+      Disabled: true
     },
     {
       Label: "Spanish",
       Value: "123-Spanish",
       IconName: null,
       Tooltip: null,
-      Disabled: false,
-      Selected: true
+      Disabled: false
     }
   ];
 
@@ -59,20 +56,46 @@ export default class EsDualListBox extends LightningElement {
   @track available;
 
   connectedCallback() {
-    console.log("Elements", JSON.parse(JSON.stringify(this.elements)));
-    this.available = this.elements
-      .filter((element) => !element.Selected)
-      .map((element) =>
-        !element.Disabled
-          ? { ...element, Tooltip: element.Name }
-          : { ...element }
-      );
-    this.selected = this.elements
-      .filter((element) => element.Selected)
-      .map((element) =>
-        !element.Disabled
-          ? { ...element, Tooltip: element.Name }
-          : { ...element }
-      );
+    console.log(
+      "Available Elements",
+      JSON.parse(JSON.stringify(this.availableElements))
+    );
+    console.log(
+      "Selected Elements",
+      JSON.parse(JSON.stringify(this.selectedElements))
+    );
+    this.available = this.availableElements.map((element) =>
+      !element.Disabled
+        ? { ...element, Tooltip: element.Name, Selected: false }
+        : { ...element, Selected: false }
+    );
+    this.selected = this.selectedElements.map((element) =>
+      !element.Disabled
+        ? { ...element, Tooltip: element.Name, Selected: false }
+        : { ...element, Selected: false }
+    );
+  }
+
+  handleSelect(event) {
+    event.stopPropagation();
+    let value = event.target.getAttribute("name");
+    console.log("Value", value);
+    this.available = this.available.map((element) =>
+      element.Value === value
+        ? { ...element, Selected: true }
+        : { ...element, Selected: false }
+    );
+    this.selected = this.selected.map((element) =>
+      element.Value === value
+        ? { ...element, Selected: true }
+        : { ...element, Selected: false }
+    );
+    let selected = this.available.find((element) => element.Value === value);
+    selected =
+      selected !== undefined
+        ? selected
+        : this.selected.find((element) => element.Value === value);
+
+    console.log("Selected", JSON.parse(JSON.stringify(selected)));
   }
 }
