ad4bba186364ba88f108162fbd01f418e03171c5
ErickSixto <sixtohernandezpalomo@gmail.com>
2021-12-05 12:33:31 -0600

    Completed UI For Setup Assistant

---
+++ force-app/main/default/lwc/esSetupAssistant/esSetupAssistant.js
@@ -0,0 +1,10 @@
+/**
+ * @description       : 
+ * @author            : ErickSixto
+ * @group             : 
+ * @last modified on  : 12-05-2021
+ * @last modified by  : ErickSixto
+**/
+import { LightningElement } from 'lwc';
+
+export default class EsSetupAssistant extends LightningElement {}
