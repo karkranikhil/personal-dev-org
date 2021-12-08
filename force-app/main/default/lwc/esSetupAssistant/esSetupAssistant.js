bd81cbc16589dffa064ca630bcd94f4e6accb70e
ErickSixto <sixtohernandezpalomo@gmail.com>
2021-12-07 21:31:37 -0600

    First Milestone Completed

--- force-app/main/default/lwc/esSetupAssistant/esSetupAssistant.js
+++ force-app/main/default/lwc/esSetupAssistant/esSetupAssistant.js
@@ -9,19 +9,29 @@ import { LightningElement, api } from "lwc";
 
 export default class EsSetupAssistant extends LightningElement {
   @api completed = false;
-  @api index;
-  @api title;
-  @api description;
-  @api additionalInfo;
-  @api badge;
-  @api badgeVariant;
-  @api firstLabel;
-  @api firstValue;
-  @api firstIconName;
-  @api secondLabel;
-  @api secondValue;
-  @api secondIconName;
-  @api thirdLabel;
-  @api thirdValue;
-  @api thirdIconName;
+  @api index = 1;
+  @api title = "Item Title";
+  @api description = "Description";
+  @api additionalInfo = "Additional Info";
+  @api badge = "Badge";
+  @api badgeVariant = "default";
+  @api firstLabel = "First Label";
+  @api firstValue = "First Value";
+  @api firstIconName = "utility:add";
+  @api secondLabel = "Second Label";
+  @api secondValue = "Second Value";
+  @api secondIconName = "utility:help";
+  @api thirdLabel = "Third Label";
+  @api thirdValue = "Third Value";
+  @api thirdIconName = "utility:clear";
+
+  @api get output() {
+    return this.selectedValue;
+  }
+  selectedValue;
+
+  handleClick(event) {
+    this.selectedValue = event.target.name;
+    console.log(this.output);
+  }
 }
