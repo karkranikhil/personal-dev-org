1ce02bbc20ae1db469b10f9de353b1592e2f3b3e
ErickSixto <sixtohernandezpalomo@gmail.com>
2021-12-15 23:19:23 -0600

    Finished UI

--- force-app/main/default/lwc/esChecklistTabset/esChecklistTabset.js
+++ force-app/main/default/lwc/esChecklistTabset/esChecklistTabset.js
@@ -1,8 +1,38 @@
 import { LightningElement, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import getContacts from "@salesforce/apex/EsChecklistController.getContacts";
+const columns = [
+  {
+    label: "Id",
+    fieldName: "urlId",
+    type: "url",
+    typeAttributes: {
+      label: { fieldName: "Id" },
+      target: "_self"
+    }
+  },
+  { label: "Name", fieldName: "Name", type: "text" },
+  { label: "Phone", fieldName: "Phone", type: "phone" },
+  { label: "Email", fieldName: "Email", type: "email" }
+];
 export default class EsChecklistTabset extends LightningElement {
   @api recordId;
+  columns = columns;
+  data;
   loading = true;
+
+  connectedCallback() {
+    getContacts({ recordId: this.recordId }).then((result) => {
+      console.log("Case Contacts", result);
+      this.data = result.map((record) => ({
+        ...record,
+        urlId: "/" + record.Id,
+        Name: record.Contact__r.Name,
+        Email: record.Contact__r.Email,
+        Phone: record.Contact__r.Phone
+      }));
+    });
+  }
 
   renderedCallback() {
     this.setLoading(false);
