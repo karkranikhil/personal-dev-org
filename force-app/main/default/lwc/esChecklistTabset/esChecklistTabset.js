c3f3e85be0727268e0f93eb8562578121a1e95ea
ErickSixto <sixtohernandezpalomo@gmail.com>
2021-12-19 14:37:24 -0600

    Need to update fields

--- force-app/main/default/lwc/esChecklistTabset/esChecklistTabset.js
+++ force-app/main/default/lwc/esChecklistTabset/esChecklistTabset.js
@@ -2,6 +2,7 @@ import { LightningElement, api, track } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getContacts from "@salesforce/apex/EsChecklistController.getContacts";
 import getRelatedRecordMap from "@salesforce/apex/EsChecklistController.getRelatedRecordMap";
+import saveDraftValues from "@salesforce/apex/EsChecklistController.saveDraftValues";
 const columns = [
   {
     label: "Id",
@@ -24,25 +25,36 @@ export default class EsChecklistTabset extends LightningElement {
   loading = true;
 
   connectedCallback() {
+    this.getRelatedRecords();
+    this.getFinalChecks();
+  }
+
+  renderedCallback() {
+    this.setLoading(false);
+  }
+
+  //* Data to be rendered at tab 2 (table)
+  getRelatedRecords() {
     getContacts({ recordId: this.recordId }).then((result) => {
       this.data = result.map((record) => ({
         ...record,
         //? Map the fields to the ones described on the 'colums' const
-        urlId: "/" + record.Id,
+        Id: record.Contact__c, //! CHANGE THE ID OF THE JUNCTION OBJECT WITH THE ID OF THE TARGET
+        urlId: "/" + record.Contact__c,
         Name: record.Contact__r.Name,
         Email: record.Contact__r.Email,
         Phone: record.Contact__r.Phone
         //? ... Here you can map more fields if needed
       }));
     });
+  }
+
+  //* Get related record that contains the checkboxes for tab 3
+  getFinalChecks() {
     getRelatedRecordMap({ recordId: this.recordId }).then((result) => {
       this.relatedRecord = result;
       console.log(result);
     });
-  }
-
-  renderedCallback() {
-    this.setLoading(false);
   }
 
   //* Form Handling
@@ -53,7 +65,21 @@ export default class EsChecklistTabset extends LightningElement {
   //* Table Handling
   handleSave(event) {
     this.saveDraftValues = event.detail.draftValues;
-    console.log(event.detail.draftValues);
+    saveDraftValues({ data: this.saveDraftValues })
+      .then(() => {
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Success",
+            message: "Records updated successfully",
+            variant: "success"
+          })
+        );
+        //Get the updated list.
+        this.getRelatedRecords();
+      })
+      .catch((error) => {
+        console.log("error : " + JSON.stringify(error));
+      });
   }
 
   //* Toasts
