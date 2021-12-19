6d18b860a91892e966a75bb5cdf2256c957f3311
ErickSixto <sixtohernandezpalomo@gmail.com>
2021-12-19 14:14:00 -0600

    Finished 3rd Tab

--- force-app/main/default/lwc/esChecklistTabset/esChecklistTabset.js
+++ force-app/main/default/lwc/esChecklistTabset/esChecklistTabset.js
@@ -1,6 +1,7 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, track } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getContacts from "@salesforce/apex/EsChecklistController.getContacts";
+import getRelatedRecordMap from "@salesforce/apex/EsChecklistController.getRelatedRecordMap";
 const columns = [
   {
     label: "Id",
@@ -11,26 +12,32 @@ const columns = [
       target: "_self"
     }
   },
-  { label: "Name", fieldName: "Name", type: "text" },
-  { label: "Phone", fieldName: "Phone", type: "phone" },
-  { label: "Email", fieldName: "Email", type: "email" }
+  { label: "Name", fieldName: "Name", type: "text", editable: true },
+  { label: "Phone", fieldName: "Phone", type: "phone", editable: true },
+  { label: "Email", fieldName: "Email", type: "email", editable: true }
 ];
 export default class EsChecklistTabset extends LightningElement {
   @api recordId;
+  @track data;
+  @track relatedRecord; //* Used for tab 3
   columns = columns;
-  data;
   loading = true;
 
   connectedCallback() {
     getContacts({ recordId: this.recordId }).then((result) => {
-      console.log("Case Contacts", result);
       this.data = result.map((record) => ({
         ...record,
+        //? Map the fields to the ones described on the 'colums' const
         urlId: "/" + record.Id,
         Name: record.Contact__r.Name,
         Email: record.Contact__r.Email,
         Phone: record.Contact__r.Phone
+        //? ... Here you can map more fields if needed
       }));
+    });
+    getRelatedRecordMap({ recordId: this.recordId }).then((result) => {
+      this.relatedRecord = result;
+      console.log(result);
     });
   }
 
@@ -42,6 +49,14 @@ export default class EsChecklistTabset extends LightningElement {
   handleSubmit() {
     this.setLoading(true);
   }
+
+  //* Table Handling
+  handleSave(event) {
+    this.saveDraftValues = event.detail.draftValues;
+    console.log(event.detail.draftValues);
+  }
+
+  //* Toasts
   handleSuccess() {
     this.dispatchEvent(
       new ShowToastEvent({
