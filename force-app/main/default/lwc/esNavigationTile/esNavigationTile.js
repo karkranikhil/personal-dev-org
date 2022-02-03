595bd03603209af88aa346cfe49e876e885b2447
ErickSixto <sixtohernandezpalomo@gmail.com>
2022-02-02 19:04:36 -0600

    Completed Base Tile

--- force-app/main/default/lwc/esNavigationTile/esNavigationTile.js
+++ force-app/main/default/lwc/esNavigationTile/esNavigationTile.js
@@ -1,8 +1,13 @@
 import { LightningElement, api } from "lwc";
+import ASSETS from "@salesforce/resourceUrl/esComponentAssets";
 
 export default class EsNavigationTile extends LightningElement {
   @api title;
   @api description;
-  @api imagePath;
+  @api imagePath = ASSETS + "/navTiles/Info.jpg";
   @api navigations;
+
+  connectedCallback() {
+    console.log("NavTile", this.imagePath);
+  }
 }
