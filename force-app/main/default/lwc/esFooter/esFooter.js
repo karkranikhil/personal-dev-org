e3f78f8c70851bb4f30586b4136076f534a61bca
ErickSixto <sixtohernandezpalomo@gmail.com>
2022-02-12 13:40:38 -0600

    Footer Styling Finish - Responsiveness

--- force-app/main/default/lwc/esFooter/esFooter.js
+++ force-app/main/default/lwc/esFooter/esFooter.js
@@ -11,4 +11,22 @@ export default class EsFooter extends LightningElement {
   @api firstNavigation;
   @api secondNavigation;
   @api fourthNavigation;
+
+  //* ---------------------- UTILITY METHODS ------------------------//
+
+  handleCollapse(event) {
+    let collapsible = event.target;
+    console.log(collapsible.classList);
+    console.log(window.innerWidth);
+    if (!collapsible.classList.contains("header") || window.innerWidth >= 768) {
+      return;
+    }
+    collapsible.classList.toggle("active");
+    let content = collapsible.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
 }
