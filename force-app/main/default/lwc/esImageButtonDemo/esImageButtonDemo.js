0482b3adbfe75952a943a1f64f65916488fa5acb
ErickSixto <sixtohernandezpalomo@gmail.com>
2022-03-06 23:21:07 -0600

    Demo Prepared

--- force-app/main/default/lwc/esImageButtonDemo/esImageButtonDemo.js
+++ force-app/main/default/lwc/esImageButtonDemo/esImageButtonDemo.js
@@ -1,3 +1,6 @@
 import { LightningElement } from "lwc";
+import IMAGE_URL from "@salesforce/resourceUrl/ImageButtonCar";
 
-export default class EsImageButtonDemo extends LightningElement {}
+export default class EsImageButtonDemo extends LightningElement {
+  imageUrl = IMAGE_URL;
+}
--- force-app/main/default/lwc/rdImageButton/rdImageButton.js
+++ force-app/main/default/lwc/rdImageButton/rdImageButton.js
@@ -1,14 +1,15 @@
 import { LightningElement, api } from "lwc";
 
 export default class RdImageButton extends LightningElement {
-  @api id;
+  @api buttonId;
   @api isMultiselect;
-  @api image = "https://picsum.photos/600?random=1";
+  @api image;
   @api theme = "light";
   @api size = "small";
   @api variant = "overlay";
   @api darkColor = "#343a40";
   @api lightColor = "#e9ecef";
+  @api label;
   state = false;
 
   //* GETTERS AND SETTERS
@@ -24,13 +25,15 @@ export default class RdImageButton extends LightningElement {
 
   renderedCallback() {
     //? Set background img
-    this.template.querySelector(".container").style.backgroundImage =
-      "url(" + this.image + ")";
+    if (this.image) {
+      this.template.querySelector(".container").style.backgroundImage =
+        "url(" + this.image + ")";
+    }
 
     //?Change Colors and Height
     let r = this.template.querySelector("*");
-    r.style.setProperty("--color-light", this.darkColor);
-    r.style.setProperty("--color-dark", this.lightColor);
+    r.style.setProperty("--color-light", this.lightColor);
+    r.style.setProperty("--color-dark", this.darkColor);
 
     //? Set classes
     let container = this.template.querySelector(".container");
@@ -38,13 +41,13 @@ export default class RdImageButton extends LightningElement {
     container.classList.add(this.size);
 
     let labelContainer = this.template.querySelector(".label-container");
-    labelContainer.style.setProperty("--color-light", this.darkColor);
-    labelContainer.style.setProperty("--color-dark", this.lightColor);
+    labelContainer.style.setProperty("--color-light", this.lightColor);
+    labelContainer.style.setProperty("--color-dark", this.darkColor);
     labelContainer.classList.add(this.theme);
     labelContainer.classList.add(this.variant);
 
     let label = this.template.querySelector(".label");
-
+    label.classList.add(this.size);
     label.classList.add(this.theme);
     label.classList.add(this.variant);
   }
