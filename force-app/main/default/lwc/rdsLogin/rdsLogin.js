aae8d74e25b7114507592271d246096682563ea0
ErickSixto <sixtohernandezpalomo@gmail.com>
2022-04-16 12:36:48 -0500

    Added color variables

--- force-app/main/default/lwc/rdsLogin/rdsLogin.js
+++ force-app/main/default/lwc/rdsLogin/rdsLogin.js
@@ -1,5 +1,37 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
 export default class RdsLogin extends LightningElement {
+  @api backgroundColor;
+  @api inactiveTabBackgroundColor;
+  @api hoverTabBackgroundColor;
+  @api tabFontColor;
+  @api fontColor;
+  @api buttonBackgroundColor;
+  @api buttonHoverBackgroundColor;
+
   expandIcon = "utility:chevrondown";
+
+  renderedCallback() {
+    if (!this.hasRendered) {
+      //?Change Colors and Height
+      let style = this.template.querySelector(".container").style;
+      style.setProperty("--color-background", this.backgroundColor);
+      style.setProperty(
+        "--color-tab-inactive-background",
+        this.inactiveTabBackgroundColor
+      );
+      style.setProperty(
+        "--color-tab-hover-background",
+        this.hoverTabBackgroundColor
+      );
+      style.setProperty("--color-tab-text", this.tabFontColor);
+      style.setProperty("--color-text", this.fontColor);
+      style.setProperty("--color-button", this.buttonBackgroundColor);
+      style.setProperty(
+        "--color-button-hover",
+        this.buttonHoverBackgroundColor
+      );
+      this.hasRendered = true;
+    }
+  }
 }
