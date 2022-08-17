bbf12988cdb669ccf35c3827a13e7cd0cd513d09
ErickSixto <sixtohernandezpalomo@gmail.com>
2022-08-17 15:45:16 -0500

    Map showing listing

--- force-app/main/default/lwc/listingMap/listingMap.js
+++ force-app/main/default/lwc/listingMap/listingMap.js
@@ -1,4 +1,26 @@
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire, track } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import CITY_FIELD from "@salesforce/schema/Listing__c.City__c";
+import STATE_FIELD from "@salesforce/schema/Listing__c.State__c";
+import ZIP_FIELD from "@salesforce/schema/Listing__c.Zip_Code__c";
+import ADDRESS_FIELD from "@salesforce/schema/Listing__c.Address__c";
+import LATITUDE_FIELD from "@salesforce/schema/Listing__c.Latitude__c";
+import LONGITUDE_FIELD from "@salesforce/schema/Listing__c.Longitude__c";
+import TITLE_FIELD from "@salesforce/schema/Listing__c.Listing_Title__c";
+import OTA_FIELD from "@salesforce/schema/Listing__c.OTA__c";
+import NAME_FIELD from "@salesforce/schema/Listing__c.Name";
+
+const FIELDS = [
+  CITY_FIELD,
+  LATITUDE_FIELD,
+  LONGITUDE_FIELD,
+  TITLE_FIELD,
+  OTA_FIELD,
+  NAME_FIELD,
+  STATE_FIELD,
+  ZIP_FIELD,
+  ADDRESS_FIELD
+];
 
 const LIST_VIEW_VISIBLE = "visible";
 const LIST_VIEW_HIDDEN = "hidden";
@@ -6,56 +28,80 @@ const DEFAULT_ZOOM = 15;
 
 export default class ListingMap extends LightningElement {
   @api recordId;
-  selectedMarkerValue = "SF1";
+  selectedMarkerValue;
   zoomLevel = DEFAULT_ZOOM;
   listViewValue = LIST_VIEW_VISIBLE;
-  center = {
-    location: { Street: "1000 5th Ave", City: "New York", State: "NY" }
-  };
+
   mapOptions = {
     disableDefaultUI: true
   };
-  mapMarkers = [
-    {
-      location: {
-        Street: "1000 5th Ave",
-        City: "New York",
-        State: "NY"
-      },
-      value: "SF1",
-      title: "Metropolitan Museum of Art",
-      description:
-        "A grand setting for one of the greatest collections of art, from ancient to contemporary."
-    },
-    {
-      location: {
-        Street: "11 W 53rd St",
-        City: "New York",
-        State: "NY"
-      },
-      value: "SF2",
+  center;
 
-      title: "Museum of Modern Art (MoMA)",
-      description: "Thought-provoking modern and contemporary art."
-    },
-    {
-      location: {
-        Street: "1071 5th Ave",
-        City: "New York",
-        State: "NY"
-      },
-      value: "SF3",
-      title: "Guggenheim Museum",
-      description: "World-renowned collection of modern and contemporary art."
-    }
-  ];
-
-  connectedCallback() {
-    console.log("Map Loaded", this.recordId);
-  }
+  @track listing;
+  @track properties;
+  @track mapMarkers;
 
   get isListHidden() {
     return this.listViewValue === LIST_VIEW_HIDDEN;
+  }
+
+  @wire(getRecord, { recordId: "$recordId", fields: FIELDS })
+  wiredRecord({ error, data }) {
+    if (error) {
+      let message = "Unknown error";
+      if (Array.isArray(error.body)) {
+        message = error.body.map((e) => e.message).join(", ");
+      } else if (typeof error.body.message === "string") {
+        message = error.body.message;
+      }
+      console.log("Error", message);
+    } else if (data) {
+      let listing = data;
+
+      this.listing = {
+        Name: listing.fields.Name.value,
+        City: listing.fields.City__c.value,
+        State: listing.fields.State__c.value,
+        Zip: listing.fields.Zip_Code__c.value,
+        Street: listing.fields.Address__c.value,
+        Latitude: listing.fields.Latitude__c.value,
+        Longitude: listing.fields.Longitude__c.value,
+        Title: listing.fields.Listing_Title__c.value,
+        OTA: listing.fields.OTA__c.value
+      };
+      this.mapMarkers = [
+        {
+          location: {
+            City: this.listing.City,
+            PostalCode: this.listing.Zip,
+            State: this.listing.State,
+            Street: this.listing.Street,
+            Latitude: this.listing.Latitude,
+            Longitude: this.listing.Longitude
+          },
+          value: this.recordId,
+          title: `Listing: ${this.listing.Title}`,
+          description: `Watchtower ID: ${this.listing.Name} - ${this.listing.OTA}`
+        }
+      ];
+      this.center = {
+        location: {
+          City: this.listing.City,
+          PostalCode: this.listing.Zip,
+          State: this.listing.State,
+          Street: this.listing.Street,
+          Latitude: this.listing.Latitude,
+          Longitude: this.listing.Longitude
+        }
+      };
+
+      console.log("LISTING", JSON.parse(JSON.stringify(this.listing)));
+    }
+  }
+
+  connectedCallback() {
+    this.selectedMarkerValue = this.recordId;
+    console.log("Map Loaded", this.recordId);
   }
 
   handleMarkerSelect(event) {
