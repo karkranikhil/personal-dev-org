00a426db7cdcd495a8c279d9d9f30f985754fa21
ErickSixto <sixtohernandezpalomo@gmail.com>
2022-12-22 15:16:33 -0600

    Created Banner and Footer

--- force-app/main/default/lwc/infinityBanner/infinityBanner.js
+++ force-app/main/default/lwc/infinityBanner/infinityBanner.js
@@ -1,17 +1,27 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 import Assets from "@salesforce/resourceUrl/infinityCRMAssets";
 
 export default class InfinityBanner extends LightningElement {
   donutIcon = Assets + "/donut.svg";
   hasRendered = false;
+  @api headline;
+  @api subtext;
+  @api backgroundColor;
+  @api fontColor;
+  @api useBackgroundPattern;
   renderedCallback() {
     if (!this.hasRendered) {
-      const banner = this.template.querySelector(".infinity-banner");
-      console.log("banner: ", JSON.parse(JSON.stringify(banner)));
-
-      banner.style.backgroundImage = `url(${this.donutIcon})`;
-      console.log("banner.style: ", banner.style);
+      this.applyBannerStyles();
     }
     this.hasRendered = true;
+  }
+
+  applyBannerStyles() {
+    const banner = this.template.querySelector(".infinity-banner");
+    if (this.useBackgroundPattern) {
+      banner.style.backgroundImage = `url(${this.donutIcon})`;
+    }
+    banner.style.setProperty("--color-background", this.backgroundColor);
+    banner.style.setProperty("--color-text", this.fontColor);
   }
 }
---
+++ force-app/main/default/lwc/infinityFooter/infinityFooter.js
@@ -0,0 +1,113 @@
+import { LightningElement, api, track } from "lwc";
+import getNavigationItems from "@salesforce/apex/esNavigationController.getNavigationItems";
+import BASE_PATH from "@salesforce/community/basePath";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { NavigationMixin } from "lightning/navigation";
+
+export default class InfinityFooter extends LightningElement {
+  @api backgroundColor;
+  @api fontColor;
+  @api icon;
+  @api iconLink;
+  @api navigation;
+
+  @track navigationItems;
+  imagePath;
+  hasRendered = false;
+  //* ---------------------- LIFECYCLE METHODS ----------------------//
+
+  connectedCallback() {
+    this.baseURL = window.location.origin + BASE_PATH;
+    this.setNavigationItems();
+  }
+
+  renderedCallback() {
+    if (!this.hasRendered) {
+      //?Change Colors and Height
+      let footer = this.template.querySelector(".footer");
+      footer.style.setProperty("--color-background", this.backgroundColor);
+      footer.style.setProperty("--color-text", this.fontColor);
+
+      this.hasRendered = true;
+    }
+  }
+
+  //* ---------------------- UTILITY METHODS ------------------------//
+
+  navigate(event) {
+    event.stopPropagation();
+    let id = event.target.name;
+
+    let nav = this.navigationItems.find((item) => item.Id === id);
+
+    switch (nav.Type) {
+      case "SalesforceObject":
+        this.navigateToObject(nav.Target);
+        break;
+      case "InternalLink":
+        window.location.href += nav.Target.replace("/", "");
+        break;
+      case "ExternalLink":
+        window.location.href = nav.Target;
+        break;
+      case "Event":
+        if (nav.Target === "Logout") {
+          this.logout();
+        }
+        if (nav.Target === "Login") {
+          this.login();
+        }
+        window.location.href = nav.Target;
+        break;
+      default:
+        break;
+    }
+  }
+
+  navigateToObject(object) {
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: object,
+        actionName: "home"
+      }
+    });
+  }
+  logout() {
+    this[NavigationMixin.Navigate]({
+      type: "comm__loginPage",
+      attributes: {
+        actionName: "logout"
+      }
+    });
+  }
+  login() {
+    this[NavigationMixin.Navigate]({
+      type: "comm__loginPage",
+      attributes: {
+        actionName: "login"
+      }
+    });
+  }
+
+  notifyUser(title, message, variant) {
+    const toastEvent = new ShowToastEvent({ title, message, variant });
+    this.dispatchEvent(toastEvent);
+  }
+
+  setNavigationItems() {
+    getNavigationItems({ NavigationDeveleoperName: this.navigation }).then(
+      (response) => {
+        if (response.length > 8) {
+          this.notifyUser(
+            "Error",
+            "Cannot set more than 8 Navigation Items - " + this.navigation,
+            "error"
+          );
+        } else {
+          this.navigationItems = [...response];
+        }
+      }
+    );
+  }
+}
