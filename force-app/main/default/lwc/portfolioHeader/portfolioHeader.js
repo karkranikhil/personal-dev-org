36e8a47a9c4ba196bd718e36dc244e741efef55b
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-01-01 15:35:08 -0600

    Progress on heros

--- force-app/main/default/lwc/portfolioHeader/portfolioHeader.js
+++ force-app/main/default/lwc/portfolioHeader/portfolioHeader.js
@@ -1,11 +1,4 @@
 import { LightningElement, track, api } from "lwc";
-import getNavigationItems from "@salesforce/apex/esNavigationController.getNavigationItems";
-
-import BASE_PATH from "@salesforce/community/basePath";
-
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
-
-import { NavigationMixin } from "lightning/navigation";
 
 export default class PortfolioHeader extends LightningElement {
   @api textColor;
@@ -44,84 +37,12 @@ export default class PortfolioHeader extends LightningElement {
       this.hasRendered = true;
     }
   }
-
-  navigate(event) {
-    event.stopPropagation();
-
-    let id = event.target.name;
-
-    let nav = this.navigationItems.find((item) => item.Id === id);
-    if (!nav)
-      nav = this.secondaryNavigationItems.find((item) => item.Id === id);
-
-    switch (nav.Type) {
-      case "SalesforceObject":
-        this.navigateToObject(nav.Target);
-        break;
-      case "InternalLink":
-        window.location.href =
-          window.location.origin + "/portal/s/" + nav.Target.replace("/", "");
-        break;
-      case "ExternalLink":
-        window.location.href = nav.Target;
-        break;
-      case "Event":
-        if (nav.Target === "Logout") {
-          this.logout();
-        }
-        if (nav.Target === "Login") {
-          this.login();
-        }
-        window.location.href = nav.Target;
-        break;
-      default:
-        break;
-    }
-  }
-  navigateToObject(object) {
-    this[NavigationMixin.Navigate]({
-      type: "standard__objectPage",
-      attributes: {
-        objectApiName: object,
-        actionName: "home"
-      }
-    });
-  }
-  logout() {
-    this[NavigationMixin.Navigate]({
-      type: "comm__loginPage",
-      attributes: {
-        actionName: "logout"
-      }
-    });
-  }
-  login() {
-    this[NavigationMixin.Navigate]({
-      type: "comm__loginPage",
-      attributes: {
-        actionName: "login"
-      }
-    });
-  }
-
-  notifyUser(title, message, variant) {
-    const toastEvent = new ShowToastEvent({ title, message, variant });
-    this.dispatchEvent(toastEvent);
-  }
-
-  setNavigationItems() {
-    getNavigationItems({
-      NavigationDeveleoperName: this.navigation
-    }).then((response) => {
-      if (response.length > 6) {
-        this.notifyUser(
-          "Error",
-          "Cannot set more than 6 Navigation Items - " + this.navigation,
-          "error"
-        );
-      } else {
-        this.navigationItems = [...response];
-      }
+  handleScrollClick() {
+    const topDiv = this.template.querySelector('[data-id="redDiv"]');
+    topDiv.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+      inline: "nearest"
     });
   }
 }
