33b81127c04383d80092082a44730ed457c2d9e8
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-01-02 11:44:33 -0600

    SinglePaged portfolio

--- force-app/main/default/lwc/portfolioHeros/portfolioHeros.js
+++ force-app/main/default/lwc/portfolioHeros/portfolioHeros.js
@@ -1,9 +1,64 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api, track } from "lwc";
 import Assets from "@salesforce/resourceUrl/portfolioPageAssets";
-// Example :- import TRAILHEAD_LOGO from '@salesforce/resourceUrl/trailhead_logo';
-export default class PortfolioHeros extends LightningElement {
-  imageUrl = Assets + "/MeCircle.png";
-  connectedCallback() {
-    console.log(this.imageUrl);
+import { NavigationMixin } from "lightning/navigation";
+
+export default class PortfolioHeros extends NavigationMixin(LightningElement) {
+  @api navigation;
+  meCircle = Assets + "/MeCircle.png";
+  meFull = Assets + "/MeFull.jpg";
+
+  isMenuOpen = false;
+  hasRendered = false;
+
+  renderedCallback() {
+    if (!this.hasRendered) {
+      let header = this.template.querySelector("header");
+      window.addEventListener("scroll", function () {
+        header.classList.toggle("sticky", window.scrollY > 100);
+      });
+
+      let menu = this.template.querySelector(".menu-icon");
+      let navlist = this.template.querySelector(".navlist");
+
+      menu.onclick = () => {
+        this.isMenuOpen = !this.isMenuOpen;
+        navlist.classList.toggle("open");
+      };
+      window.onscroll = () => {
+        menu.classList.remove("bx-x");
+        navlist.classList.remove("open");
+      };
+
+      this.hasRendered = true;
+    }
+  }
+
+  handleScroll(event) {
+    const hrefValue = event.target.href;
+    // Get the value of the hash from the href value.
+    const hash = hrefValue.replace("#", ".");
+    // Select the target element using the hash.
+    const targetElement = this.template.querySelector(hash);
+    // Scroll to the target element.
+    targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+  navigateToPage(event) {
+    let page = event.target.name;
+    console.log("page: ", page);
+    this[NavigationMixin.Navigate]({
+      type: "comm__namedPage",
+      attributes: {
+        name: page
+      }
+    });
+  }
+  navigateToObject(object) {
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: object,
+        actionName: "home"
+      }
+    });
   }
 }
