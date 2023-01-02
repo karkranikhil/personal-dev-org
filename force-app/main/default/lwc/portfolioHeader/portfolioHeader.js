02a00470bf49b63bbe6954c0d6e98f7a7a470492
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-01-02 02:35:35 -0600

    Updated header to work with navigation

--- force-app/main/default/lwc/portfolioHeader/portfolioHeader.js
+++ force-app/main/default/lwc/portfolioHeader/portfolioHeader.js
@@ -1,6 +1,9 @@
 import { LightningElement, track, api } from "lwc";
-
-export default class PortfolioHeader extends LightningElement {
+import getNavigationItems from "@salesforce/apex/esNavigationController.getNavigationItems";
+import BASE_PATH from "@salesforce/community/basePath";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { NavigationMixin } from "lightning/navigation";
+export default class PortfolioHeader extends NavigationMixin(LightningElement) {
   @api textColor;
   @api mainColor;
   @api navigation;
@@ -9,6 +12,7 @@ export default class PortfolioHeader extends LightningElement {
   isMenuOpen = false;
 
   connectedCallback() {
+    this.baseURL = window.location.origin + BASE_PATH;
     this.setNavigationItems();
   }
 
@@ -37,12 +41,96 @@ export default class PortfolioHeader extends LightningElement {
       this.hasRendered = true;
     }
   }
-  handleScrollClick() {
-    const topDiv = this.template.querySelector('[data-id="redDiv"]');
-    topDiv.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest"
+  navigate(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    let id = event.target.name;
+    console.log("id: ", id);
+
+    let nav = this.navigationItems.find((item) => item.Id === id);
+    console.log("nav: ", JSON.parse(JSON.stringify(nav)));
+    let pageName = "home";
+
+    switch (nav.Type) {
+      case "SalesforceObject":
+        this.navigateToObject(nav.Target);
+        break;
+      case "InternalLink":
+        pageName = nav.Target.replace("/", "")
+          ? nav.Target.replace("/", "").replace("-", "_") + "__c"
+          : "Home";
+        this.navigateToPage(pageName);
+        break;
+      case "ExternalLink":
+        window.location.href = nav.Target;
+        break;
+      case "Event":
+        if (nav.Target === "Logout") {
+          this.logout();
+        }
+        if (nav.Target === "Login") {
+          this.login();
+        }
+        window.location.href = nav.Target;
+        break;
+      default:
+        break;
+    }
+  }
+  navigateToPage(page) {
+    console.log("page: ", page);
+    this[NavigationMixin.Navigate]({
+      type: "comm__namedPage",
+      attributes: {
+        name: page
+      }
+    });
+  }
+  navigateToObject(object) {
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: object,
+        actionName: "home"
+      }
+    });
+  }
+  logout() {
+    this[NavigationMixin.Navigate]({
+      type: "comm__loginPage",
+      attributes: {
+        actionName: "logout"
+      }
+    });
+  }
+  login() {
+    this[NavigationMixin.Navigate]({
+      type: "comm__loginPage",
+      attributes: {
+        actionName: "login"
+      }
+    });
+  }
+
+  notifyUser(title, message, variant) {
+    const toastEvent = new ShowToastEvent({ title, message, variant });
+    this.dispatchEvent(toastEvent);
+  }
+
+  setNavigationItems() {
+    getNavigationItems({
+      NavigationDeveleoperName: this.navigation
+    }).then((response) => {
+      if (response.length > 6) {
+        this.notifyUser(
+          "Error",
+          "Cannot set more than 6 Navigation Items - " + this.navigation,
+          "error"
+        );
+      } else {
+        this.navigationItems = [...response];
+      }
     });
   }
 }
