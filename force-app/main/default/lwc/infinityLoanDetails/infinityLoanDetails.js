3a2b3f12e51c43e6c5ca8e29b79cb58ad781be56
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-01-14 11:30:29 -0600

    Completed Loan Details Table

---
+++ force-app/main/default/lwc/infinityLoanDetails/infinityLoanDetails.js
@@ -0,0 +1,29 @@
+import { api, track, LightningElement } from "lwc";
+import getLoanByRecordId from "@salesforce/apex/InfinityCurrentLoansController.getLoanByRecordId";
+
+export default class InfinityLoanDetails extends LightningElement {
+  @api recordId;
+  @track loan = null;
+  @track error;
+  isLoading = true;
+
+  connectedCallback() {
+    console.log("@@recordId", this.recordId);
+    this.handleFetch();
+  }
+  handleFetch() {
+    getLoanByRecordId({ recordId: this.recordId })
+      .then((result) => {
+        console.log("@@ Loan Data", JSON.parse(JSON.stringify(result)));
+        this.loan = { ...result };
+        console.log("this.loan: ", JSON.parse(JSON.stringify(this.loan)));
+      })
+      .catch((error) => {
+        console.log("@@error: ", JSON.parse(JSON.stringify(error)));
+        this.load = null;
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+}
