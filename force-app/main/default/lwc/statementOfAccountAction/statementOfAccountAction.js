ee3e9bd89e0fe507b62f9d140fa4851a1288a5e9
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-04-02 12:04:33 -0600

    added pdfQuickAction

---
+++ force-app/main/default/lwc/statementOfAccountAction/statementOfAccountAction.js
@@ -0,0 +1,36 @@
+import { LightningElement, api, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import FIRSTNAME_FIELD from "@salesforce/schema/Contact.FirstName";
+import LASTNAME_FIELD from "@salesforce/schema/Contact.LastName";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { CloseActionScreenEvent } from "lightning/actions";
+
+export default class StatementOfAccountAction extends LightningElement {
+  @api recordId;
+  @api objectApiName;
+
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [FIRSTNAME_FIELD, LASTNAME_FIELD]
+  })
+  contact;
+
+  get firstName() {
+    return this.contact.data ? this.contact.data.fields.FirstName.value : null;
+  }
+
+  get lastName() {
+    return this.contact.data ? this.contact.data.fields.LastName.value : null;
+  }
+  handleSuccess(e) {
+    // Close the modal window and display a success toast
+    this.dispatchEvent(new CloseActionScreenEvent());
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: "Success",
+        message: "Method activated",
+        variant: "info"
+      })
+    );
+  }
+}
