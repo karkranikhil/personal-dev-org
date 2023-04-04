9a4426bdfc63b85a863466ca046a55bc33b8ac69
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-04-04 17:48:38 -0600

    No data message and loading animation

--- force-app/main/default/lwc/statementOfAccountAction/statementOfAccountAction.js
+++ force-app/main/default/lwc/statementOfAccountAction/statementOfAccountAction.js
@@ -1,41 +1,48 @@
-import { LightningElement, api, wire } from "lwc";
+import { LightningElement, api, wire, track } from "lwc";
 import { getRecord } from "lightning/uiRecordApi";
 import FIRSTNAME_FIELD from "@salesforce/schema/Contact.FirstName";
 import LASTNAME_FIELD from "@salesforce/schema/Contact.LastName";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import { CloseActionScreenEvent } from "lightning/actions";
+import isValid from "@salesforce/apex/StatementOfAccountController.isValid";
 
 export default class StatementOfAccountAction extends LightningElement {
   @api recordId;
   @api objectApiName;
+  @track contact;
+  isValid = false;
+  isLoading = true;
 
   @wire(getRecord, {
     recordId: "$recordId",
     fields: [FIRSTNAME_FIELD, LASTNAME_FIELD]
   })
-  contact;
+  wiredContact({ error, data }) {
+    if (error) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          title: "Error",
+          message: "There was an error getting the contact information.",
+          variant: "error"
+        })
+      );
+    } else if (data) {
+      this.contact = { ...data };
+      this.isValid = isValid({ contactId: this.recordId }).finally(
+        () => (this.isLoading = false)
+      );
+    }
+  }
 
   get firstName() {
-    return this.contact.data ? this.contact.data.fields.FirstName.value : null;
+    return this.contact ? this.contact.fields.FirstName.value : null;
   }
 
   get lastName() {
-    return this.contact.data ? this.contact.data.fields.LastName.value : null;
+    return this.contact ? this.contact.fields.LastName.value : null;
   }
 
   get invoicePdfUrl() {
     return `/apex/statementOfAccountPDF?contactId=${this.recordId}`;
-  }
-
-  handleSuccess(e) {
-    // Close the modal window and display a success toast
-    this.dispatchEvent(new CloseActionScreenEvent());
-    this.dispatchEvent(
-      new ShowToastEvent({
-        title: "Success",
-        message: "Method activated",
-        variant: "info"
-      })
-    );
   }
 }
