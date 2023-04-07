75b36ef2673c266c6e1b41b70aa42f9acd8b647c
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-04-07 17:41:29 -0600

    Completed site

---
+++ force-app/main/default/lwc/propernyeContent/propernyeContent.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+import RESOURCES from "@salesforce/resourceUrl/propernyeResources";
+
+export default class PropernyeContent extends LightningElement {
+  imageUrl = RESOURCES + "/images/proper-banner.jpg";
+}
