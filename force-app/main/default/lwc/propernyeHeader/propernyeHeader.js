bbd14c3cfec887effbeb1a12465ef21670d56393
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-04-07 14:16:57 -0600

    Created header for propernye, not dynamic yet

---
+++ force-app/main/default/lwc/propernyeHeader/propernyeHeader.js
@@ -0,0 +1,132 @@
+import { LightningElement, api, track, wire } from "lwc";
+import RESOURCES from "@salesforce/resourceUrl/propernyeResources";
+import getNavigationItems from "@salesforce/apex/esNavigationController.getNavigationItems";
+import BASE_PATH from "@salesforce/community/basePath";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { NavigationMixin } from "lightning/navigation";
+import USER_ID from "@salesforce/user/Id";
+import IS_GUEST from "@salesforce/user/isGuest";
+import { getRecord } from "lightning/uiRecordApi";
+export default class PropernyeHeader extends NavigationMixin(LightningElement) {
+  @track navigationItems = [];
+  @api navigation;
+  @api recordId;
+
+  logoUrl = RESOURCES + "/images/logo.png";
+  iconUrl = RESOURCES + "/images/fingers.png";
+  closeIcon = RESOURCES + "/images/close.svg";
+
+  userId = USER_ID;
+  isGuest = IS_GUEST;
+  userName;
+  hasRendered = false;
+
+  @wire(getRecord, { recordId: "$userId", fields: ["User.Name"] })
+  wiredUser({ error, data }) {
+    if (data) {
+      console.log("@@ UserData: ", data);
+      this.userName = data.fields.Name.value;
+    }
+  }
+
+  //* LIFE CYCLE
+  connectedCallback() {
+    this.baseURL = window.location.origin + BASE_PATH;
+    this.setNavigationItems();
+  }
+
+  //* NAVIGATION
+
+  navigate(event) {
+    event.stopPropagation();
+
+    let id = event.target.name;
+
+    let nav = this.navigationItems.find((item) => item.Id === id);
+
+    switch (nav.Type) {
+      case "SalesforceObject":
+        this.navigateToObject(nav.Target);
+        break;
+      case "InternalLink":
+        window.location.href =
+          window.location.origin + "/portal/s/" + nav.Target.replace("/", "");
+        break;
+      case "ExternalLink":
+        window.location.href = nav.Target;
+        break;
+      case "Event":
+        if (nav.Target === "Logout") {
+          this.logout();
+        }
+        if (nav.Target === "Login") {
+          this.login();
+        }
+        window.location.href = nav.Target;
+        break;
+      default:
+        break;
+    }
+  }
+  navigateToObject(object) {
+    this[NavigationMixin.Navigate]({
+      type: "standard__objectPage",
+      attributes: {
+        objectApiName: object,
+        actionName: "home"
+      }
+    });
+  }
+  logout() {
+    this[NavigationMixin.Navigate]({
+      type: "comm__loginPage",
+      attributes: {
+        actionName: "logout"
+      }
+    });
+  }
+  login() {
+    this[NavigationMixin.Navigate]({
+      type: "comm__loginPage",
+      attributes: {
+        actionName: "login"
+      }
+    });
+  }
+
+  notifyUser(title, message, variant) {
+    const toastEvent = new ShowToastEvent({ title, message, variant });
+    this.dispatchEvent(toastEvent);
+  }
+
+  setNavigationItems() {
+    getNavigationItems({
+      NavigationDeveleoperName: this.mainNavigation
+    }).then((response) => {
+      if (response.length > 8) {
+        this.notifyUser(
+          "Error",
+          "Cannot set more than 8 Navigation Items - " + this.mainNavigation,
+          "error"
+        );
+      } else {
+        this.navigationItems = [...response];
+      }
+    });
+  }
+
+  //* USER INTERACTION
+
+  openSideNav() {
+    const openHamMenu = this.template.querySelector(".oxy-menu-toggle");
+    const mobileMenuIcon = this.template.querySelector(".mobile__menuWrap");
+    mobileMenuIcon.classList.add("open");
+    openHamMenu.classList.add("close");
+  }
+  closeSideNav() {
+    const openHamMenu = this.template.querySelector(".oxy-menu-toggle");
+    const mobileMenuIcon = this.template.querySelector(".mobile__menuWrap");
+    mobileMenuIcon.classList.remove("open");
+    openHamMenu.classList.remove("close");
+  }
+}
