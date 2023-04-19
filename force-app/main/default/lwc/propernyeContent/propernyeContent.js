465462e5f9b0cf021c8482457fe573096250fea5
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-04-19 12:52:39 -0600

    Fixed navigation speed of content

--- force-app/main/default/lwc/propernyeContent/propernyeContent.js
+++ force-app/main/default/lwc/propernyeContent/propernyeContent.js
@@ -1,6 +1,17 @@
 import { LightningElement } from "lwc";
 import RESOURCES from "@salesforce/resourceUrl/propernyeResources";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class PropernyeContent extends LightningElement {
+export default class PropernyeContent extends NavigationMixin(LightningElement) {
   imageUrl = RESOURCES + "/images/proper-banner.jpg";
+
+  navigateToPage(event) {
+    event.preventDefault();
+    this[NavigationMixin.Navigate]({
+      type: "comm__namedPage",
+      attributes: {
+        name: "Book_a_Trip__c"
+      }
+    });
+  }
 }
