d03c462ab11048f6b0bb5a5d5e183ed65dcff756
ErickSixto <sixtohernandezpalomo@gmail.com>
2023-05-12 00:01:08 -0600

    Calling flow works

--- force-app/main/default/lwc/cartButton/cartButton.js
+++ force-app/main/default/lwc/cartButton/cartButton.js
@@ -7,24 +7,77 @@ export default class CartButton extends LightningElement {
   @api flowName = "Get_Product_Count_Invokable"; // Default flow name
   @track badgeCount = 0;
 
+  cookieName = "artSessionStorageId";
+  cookieVal = "";
+
   connectedCallback() {
+    this.checkCookies();
     this.getBadgeCount();
   }
 
   get isDisplayBadge() {
-    return this.badgeCount != null && this.badgeCount > 0;
+    return this.badgeCount != null;
+    // return this.badgeCount != null && this.badgeCount > 0;
   }
   getBadgeCount() {
-    let inputVariables = [
-      { name: "userId", type: "String", value: this.userId },
-      { name: "sessionStorageId", type: "String", value: this.sessionStorageId }
-    ];
+    let inputVariables = {
+      userID: this.userId,
+      sessionID: this.sessionStorageId
+    };
+    console.log("@@ inputVariables: ", inputVariables);
+
     getCount({ flowName: this.flowName, inputVariables: inputVariables })
       .then((result) => {
-        this.badgeCount = result;
+        console.log("@@ Cart Count: ", result);
+        this.badgeCount = Math.trunc(result);
       })
       .catch((error) => {
         console.error(error.body.message);
       });
+  }
+
+  //* Coockie Session Logic
+
+  checkCookies() {
+    var result = this.retrieveCookie();
+    console.log("found cookie?" + result);
+
+    if (result == "") {
+      var newUuid = crypto.randomUUID();
+      this.createCookie(this.cookieName, newUuid, 5);
+      this.sessionStorageId = newUuid;
+    } else {
+      console.log("cookie found!");
+      this.sessionStorageId = result;
+    }
+  }
+
+  retrieveCookie() {
+    console.log("getting cookies");
+    console.log(document.cookie);
+
+    var cookieString = "; " + document.cookie;
+    var parts = cookieString.split("; " + this.cookieName + "=");
+    return decodeURIComponent(parts.pop().split(";").shift());
+  }
+
+  createCookie(name, value, daysToLive) {
+    console.log("creating new cookie");
+
+    var expires;
+
+    if (daysToLive) {
+      const date = new Date();
+      date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+      expires = "; expires=" + date.toGMTString();
+    } else {
+      expires = "";
+    }
+
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+  deleteCookie(cookieName) {
+    this.createCookie(cookieName, "", null);
   }
 }
---
+++ force-app/main/default/lwc/portalCookie/portalCookie.js
@@ -0,0 +1,60 @@
+import { LightningElement, track, api } from 'lwc';
+
+export default class PortalCookie extends LightningElement {
+
+    @track cookieName = 'artBookingSession';
+    @track cookieVal = '';
+
+    @api bookingSession = '';
+
+    connectedCallback(){
+        this.checkCookies();
+        //this.deleteCookie(this.cookieName);
+    }
+
+    checkCookies(){
+        var result = this.retrieveCookie();
+        console.log('found cookie?' + result);
+
+        if(result == ''){
+            var newUuid = crypto.randomUUID();
+            this.createCookie(this.cookieName, newUuid, 5);
+            this.bookingSession = newUuid;
+        }
+        else{
+            console.log('cookie found!');
+            this.bookingSession = result;
+        }
+    }
+
+    retrieveCookie(){
+        console.log('getting cookies');
+        console.log(document.cookie);
+
+        var cookieString = "; " + document.cookie;
+        var parts = cookieString.split("; " + this.cookieName + "=");
+        return decodeURIComponent(parts.pop().split(";").shift());
+    }
+
+    createCookie(name, value, daysToLive){
+        console.log('creating new cookie');
+
+        var expires;
+ 
+        if (daysToLive) {
+            const date = new Date();
+            date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
+        }
+ 
+        document.cookie = name + "=" + value + expires + "; path=/";
+
+    }
+
+    deleteCookie(cookieName){
+        this.createCookie(cookieName, '', null);
+    }
+
+}
